6ad17f09f90b7652184fdc486c5bcf7be0f7fbcf
Rafael Chioca <rafaelmchioca@gmail.com>
2023-05-29 20:01:21 -0300

    Projeto Node com MongoDB

--- ProjetoNodeMongoDB/index.js
+++ ProjetoNodeMongoDB/index.js
@@ -17,8 +17,8 @@ server.use(express.json());
 server.use('/funcionario', funcionarioRoutes);
 
 // Conexão com MongoDB atlas
-const DB_USER = 'fatec_franca'
-const DB_PASSWORD = encodeURIComponent('2KTAO4YCkJJc2p2R')
+const DB_USER = 'user'
+const DB_PASSWORD = encodeURIComponent('password')
 
 mongoose.connect(
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.vo0fhaq.mongodb.net/?retryWrites=true&w=majority`
@@ -29,105 +29,5 @@ mongoose.connect(
     .catch((err) => {
         console(err);
     })
-
-// const cursos  = ['Node JS', 'JavaScript', 'PHP', 'React Show', 'VueJS'];
-
-// // Middleware Global
-// server.use((req, res, next) =>{
-//     console.log(`URL CHAMADA: ${req.url}`);
-
-//     return next();
-// });
-
-// // Middleware Local (especifico para tratar do INSERT de novos cursos)
-// function checkCurso(req, res, next){
-//     if(!req.body.novo_curso){
-//         return res.status(400).json({error: "Nome do curso é obrigatório nesse formato {'novo_nome': 'Lua'}"});
-//     }
-
-//     return next();
-// };
-
-// // Middleware Local (especifico para cursos que não existem)
-// function checkIDCurso(req, res, next){
-//     const curso = cursos[req.params.index];
-//     if(!curso){
-//         return res.status(400).json({ error: "O curso não existe no ID solicitado"});
-//     }
-
-//     return next();
-// }
-
-// // Middleware Local (especifico para request body existente)
-// function checkReqBody(req, res, next){
-//     if(!req.body.curso){
-//         return res.status(400).json({error: "Obrigatório informar curso nesse formato {'curso': 'Node JS Avançado'}"});
-//     }
-
-//     return next();
-// };
-
-// // Middleware Local (console.log lista curso)
-// function checkInsert(req, res, next){
-//     if(cursos.push){
-//         console.log(cursos);
-//     }
-
-//     return next();
-// };
-
-// // Middleware Local (especifico para mensagem no delete)
-// function checkDelete(req, res, next){
-//     if(cursos.splice){
-//         return res.status(200).json({message: "Curso deletado com sucesso"});
-//     }
-
-//     return next();
-// };
-
-
-// // Criando meu SELECT
-// server.get('/curso', (req, res) => {
-//     return res.json(cursos);
-// });
-
-// // Query params = ?nome=NodeJS
-// // Route params = /curso/2
-// //Request Body = { nome: 'Node JS', tipo: 'Backend'}
-// // localhost:3000/curso
-
-// //Criando meu SELECT passando ID
-// server.get('/curso/:index', checkIDCurso, (req, res) => {
-//     // const nome = req.query.nome;
-//     // const id = req.params.id;
-//     // return res.json({ curso: `${id}`, nome: `${nome}`});
-//     const { index } = req.params;
-//     return res.json(cursos[index]);
-// });
-
-// // Permitindo INSERIR dados via API
-// server.post('/curso', checkCurso, checkInsert, (req, res) => {
-//     const { novo_curso } = req.body;
-//     cursos.push(novo_curso);
-
-//     return res.json(cursos)
-// })
-
-// // Permitindo UPDATE de um curso
-// server.put('/curso/:index', checkIDCurso, checkReqBody, (req, res) => {
-//     const { index } = req.params;
-//     const { curso } = req.body;
-
-//     cursos[index] = curso;
-
-//     return res.json(cursos);
-// })
-
-// // Permitindo o DELETE de um curso
-// server.delete('/curso/:index', checkDelete, (req, res) => {
-//     const { index } = req.params;
-
-//     cursos.splice(index, 1);
-// })
 
 server.listen(3000);
